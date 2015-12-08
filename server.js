55ec2433cb28ba7f228a3780654ef24388df5eca
yuze wang <yuze89@gmail.com>
2015-12-08 17:19:47 +0800

    add a console log

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var http = require("http");
 var os = require("os");
 http.createServer(function(request, response) {
     console.log("New request arrived.");
+    console.log("New request add.");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Welcome to yuze's website! you are visiting " + os.hostname() + "\n");
     response.end();
