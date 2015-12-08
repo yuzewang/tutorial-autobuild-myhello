5099697138bce90a56da8c76dc1f39be4131ea6d
yuze wang <yuze89@gmail.com>
2015-12-08 16:12:50 +0800

    yuze modify

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var os = require("os");
 http.createServer(function(request, response) {
     console.log("New request arrived.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Welcome to my website! you are visiting " + os.hostname() + "\n");
+    response.write("Welcome to yuze's website! you are visiting " + os.hostname() + "\n");
     response.end();
 }).listen(80);
-console.log("Server is running, listening on port 80....");
+console.log("nodejs Server is running, listening on port 80....");
