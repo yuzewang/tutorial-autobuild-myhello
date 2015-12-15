a5eedf48490d5d71c391891ccfda22a269be32fa
yuze wang <yuze89@gmail.com>
2015-12-15 17:10:20 +0800

    modify port 9000

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var os = require("os");
 http.createServer(function(request, response) {
     console.log("New request arrived.");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Welcome to wangyuze's website! you are visiting " + os.hostname() + "\n");
+    response.write("2015-12-15 17:09:00 Welcome to wangyuze's website! you are visiting " + os.hostname() + "\n");
     response.end();
-}).listen(80);
+}).listen(9000);
 console.log("nodejs Server is running, listening on port 80....");
